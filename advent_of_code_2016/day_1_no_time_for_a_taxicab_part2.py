58a32dd97c5674f1802c76b0a603fb7314948380
Charlie Liang Yuan <charlie.liang.yuan@gmail.com>
2017-11-27 18:57:41 -0800

    solve part 2 and rename

---
+++ advent_of_code_2016/day_1_no_time_for_a_taxicab_part2.py
@@ -0,0 +1,60 @@
+"""
+You're airdropped near Easter Bunny Headquarters in a city somewhere. "Near", unfortunately, is as close as you can get - the instructions on the Easter Bunny Recruiting Document the Elves intercepted start here, and nobody had time to work them out further.
+
+The Document indicates that you should start at the given coordinates (where you just landed) and face North. Then, follow the provided sequence: either turn left (L) or right (R) 90 degrees, then walk forward the given number of blocks, ending at a new intersection.
+
+There's no time to follow such ridiculous instructions on foot, though, so you take a moment and work out the destination. Given that you can only walk on the street grid of the city, how far is the shortest path to the destination?
+
+For example:
+
+Following R2, L3 leaves you 2 blocks East and 3 blocks North, or 5 blocks away.
+R2, R2, R2 leaves you 2 blocks due South of your starting position, which is 2 blocks away.
+R5, L5, R5, R3 leaves you 12 blocks away.
+How many blocks away is Easter Bunny HQ?
+
+--- Part Two ---
+
+Then, you notice the instructions continue on the back of the Recruiting Document. Easter Bunny HQ is actually at the first location you visit twice.
+
+For example, if your instructions are R8, R4, R4, R8, the first location you visit twice is 4 blocks away, due East.
+
+How many blocks away is the first location you visit twice?
+"""
+
+input = 'L4, L3, R1, L4, R2, R2, L1, L2, R1, R1, L3, R5, L2, R5, L4, L3, R2, R2, L5, L1, R4, L1, R3, L3, R5, R2, L5, R2, R1, R1, L5, R1, L3, L2, L5, R4, R4, L2, L1, L1, R1, R1, L185, R4, L1, L1, R5, R1, L1, L3, L2, L1, R2, R2, R2, L1, L1, R4, R5, R53, L1, R1, R78, R3, R4, L1, R5, L1, L4, R3, R3, L3, L3, R191, R4, R1, L4, L1, R3, L1, L2, R3, R2, R4, R5, R5, L3, L5, R2, R3, L1, L1, L3, R1, R4, R1, R3, R4, R4, R4, R5, R2, L5, R1, R2, R5, L3, L4, R1, L5, R1, L4, L3, R5, R5, L3, L4, L4, R2, R2, L5, R3, R1, R2, R5, L5, L3, R4, L5, R5, L3, R1, L1, R4, R4, L3, R2, R5, R1, R2, L1, R4, R1, L3, L3, L5, R2, R5, L1, L4, R3, R3, L3, R2, L5, R1, R3, L3, R2, L1, R4, R3, L4, R5, L2, L2, R5, R1, R2, L4, L4, L5, R3, L4'
+
+from sets import Set
+
+# Probably better to use the lines and look for intersection.
+# But here we just put all points into a map.
+steps = input.split(', ')
+
+def find_first_duplicate_location(steps):
+    direction = 0
+    location = (0, 0)
+    points = Set([location])
+
+    for step in steps:
+        direction = (direction + (1 if step[0] == 'R' else -1)) % 4
+        for i in range(0, int(step[1:])):
+            if direction == 0:
+                location = (location[0], location[1] + 1)
+            elif direction == 1:
+                location = (location[0] + 1, location[1])
+            elif direction == 2:
+                location = (location[0], location[1] - 1)
+            else:
+                location = (location[0] - 1, location[1])
+            
+            if location in points:
+                return location
+            else:
+                points.add(location)
+
+assert(find_first_duplicate_location(['R2', 'R1', 'R1', 'R2'])) == (1, 0)
+
+result = find_first_duplicate_location(steps)
+
+distance = abs(result[0]) + abs(result[1])
+
+print distance
